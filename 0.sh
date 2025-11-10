0810d69ba9cace382ea33c2bba873903cf4a443d
code-jpn <haruto_24s1200197@nnn.ed.jp>
2025-11-10 13:42:42 +0900

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export TZ=Asia/Tokyo
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
-convert -size 400x100 xc:lightblue -gravity center \
+convert -size 400x100 xc:white -gravity center \
   -pointsize 24 -annotate 0 "$DATE" clock.png

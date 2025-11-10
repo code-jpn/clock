f2dfd5393c17519d79a697a6ab274246f13c0bc5
code-jpn <haruto_24s1200197@nnn.ed.jp>
2025-11-10 18:49:07 +0900

    Update 0.sh

--- 0.sh
+++ 0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export TZ=Asia/Tokyo
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
-convert -background transparent -fill black -pointsize 20 \
-label:"$DATE" clock.png
+convert -background transparent -fill black -pointsize 100 \
+label:"$DATE" -trim clock.png
